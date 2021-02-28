5b2da1488885fb22626b4fde46e177efd352a224
wolfysacc <65271254+wolfysacc@users.noreply.github.com>
2021-02-28 14:14:11 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,8 +1,6 @@
 local wolfysStuff = Instance.new("ScreenGui")
 
-syn.set_thread_identity(5)
 syn.protect(wolfysStuff)
-wolfysStuff.Parent = game:GetService("CoreGui")
 
 local _BACKGROUND_ = Instance.new("Frame")
 local VISUALIZER = Instance.new("TextLabel")
